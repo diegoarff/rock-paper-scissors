fab24d15963864c33ee619aaf5bca0b23991f02a
diegoarff <diegorinconf1@gmail.com>
2023-01-11 14:38:32 -0400

    Add round functionality and display result

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const possibleChoices = {
-	rock: { weakTo: 'paper', strongTo: 'scissors' },
-	paper: { weakTo: 'scissors', strongTo: 'rock' },
-	scissors: { weakTo: 'rock', strongTo: 'paper' },
+	rock: { beats: 'scissors' },
+	paper: { beats: 'rock' },
+	scissors: { beats: 'paper' },
 };
 
 function getComputerChoice() {
@@ -17,7 +17,53 @@ function getUserChoice() {
 	return userInput.toLowerCase();
 }
 
+function getResult(userChoice, computerChoice) {
+	if (userChoice === computerChoice) {
+		return {
+			message: "It's a tie!",
+			color: 'orange',
+		};
+	}
+
+	if (possibleChoices[userChoice].beats === computerChoice) {
+		return {
+			message: `User wins. ${userChoice} beats ${computerChoice}.`,
+			color: 'green',
+		};
+	}
+
+	return {
+		message: `Computer wins. ${userChoice} is defeated by ${computerChoice}.`,
+		color: 'red',
+	};
+}
+
 function displayChoices(userChoice, computerChoice) {
+	console.log('CHOICES:');
 	console.log('%cYou chose: ' + userChoice, 'color: cyan');
 	console.log('%cComputer chose: ' + computerChoice, 'color: pink');
 }
+
+function displayResult(result) {
+	const { message, color } = result;
+	console.log(
+		`%c${message}`,
+		`color: ${color}; background: white; font-style: italic;`
+	);
+}
+
+function playRound() {
+	const userChoice = getUserChoice();
+	const computerChoice = getComputerChoice();
+
+	if (userChoice === null) {
+		return;
+	}
+
+	const result = getResult(userChoice, computerChoice);
+
+	displayChoices(userChoice, computerChoice);
+	displayResult(result);
+}
+
+playRound();
