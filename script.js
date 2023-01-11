16d1eb9bfe33265606518a4f8071d4b0c2e86f39
diegoarff <diegorinconf1@gmail.com>
2023-01-11 14:22:20 -0400

    Create functions to get computer and user choices

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('Hello World');
+const possibleChoices = {
+	rock: { weakTo: 'paper', strongTo: 'scissors' },
+	paper: { weakTo: 'scissors', strongTo: 'rock' },
+	scissors: { weakTo: 'rock', strongTo: 'paper' },
+};
+
+function getComputerChoice() {
+	const choices = Object.keys(possibleChoices);
+	return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function getUserChoice() {
+	const userInput = prompt('Rock, Paper or Scissors?');
+	if (userInput === null) {
+		return null;
+	}
+	return userInput.toLowerCase();
+}
+
+function displayChoices(userChoice, computerChoice) {
+	console.log('%cYou chose: ' + userChoice, 'color: cyan');
+	console.log('%cComputer chose: ' + computerChoice, 'color: pink');
+}
